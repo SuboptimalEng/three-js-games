b184698306f96aabc251c024b715963be008dd76
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-11-18 14:20:30 -0600

    Add button to toggle custom code mirror editor.

---
+++ brick-breaker/pages/components/CustomEditor.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Draggable from "react-draggable";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+
+export default function CustomEditor() {
+  const codeArray = [
+    `// 🐦 twitter.com/SuboptimalEng
+// 💻 github.com/SuboptimalEng/GameDev
+
+const bg = new THREE.BoxGeometry(8, 8, 8);
+const bm = new THREE.MeshNormalMaterial();
+const b = new THREE.Mesh(bg, bm);
+test.scene.add(b);
+
+const animate = () => {
+  b.rotation.x += 0.001;
+  b.rotation.y += 0.001;
+  window.requestAnimationFrame(animate);
+};
+  `,
+  ];
+  const [code, setCode] = useState(codeArray[0]);
+
+  return (
+    <div className="absolute inset-x-64 inset-y-32">
+      <Draggable>
+        <div className="w-full h-full text-2xl bg-white rounded-xl">
+          <div className="w-full h-full p-2">
+            <CodeMirror
+              value={code}
+              height="100%"
+              theme="light"
+              extensions={[javascript()]}
+              onChange={(value, viewUpdate) => {
+                console.log("value:", value);
+                setCode(value);
+              }}
+              className="h-full w-11/12"
+            />
+            <div className="absolute border top-2 bottom-2 right-2 w-1/12">
+              <div className="flex flex-col h-full place-items-center justify-center">
+                <button onClick={() => console.log("hi")}>hi</button>
+                <button onClick={() => console.log("there")}>there</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Draggable>
+    </div>
+  );
+}
--- brick-breaker/pages/index.jsx
+++ brick-breaker/pages/index.jsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from "react";
 import * as THREE from "three";
-import Draggable from "react-draggable";
-import CodeMirror from "@uiw/react-codemirror";
-import { javascript } from "@codemirror/lang-javascript";
 
 import SceneInit from "./lib/SceneInit";
+import CustomEditor from "./components/CustomEditor";
 
 export default function Home() {
   useEffect(() => {
@@ -26,49 +24,17 @@ export default function Home() {
     animate();
   }, []);
 
-  const codeArray = [
-    `// 🐦 twitter.com/SuboptimalEng
-// 💻 github.com/SuboptimalEng/GameDev
-
-const bg = new THREE.BoxGeometry(8, 8, 8);
-const bm = new THREE.MeshNormalMaterial();
-const b = new THREE.Mesh(bg, bm);
-test.scene.add(b);
-
-const animate = () => {
-  b.rotation.x += 0.001;
-  b.rotation.y += 0.001;
-  window.requestAnimationFrame(animate);
-};
-  `,
-  ];
-  const [code, setCode] = useState(codeArray[0]);
+  const [showCustomEditor, setShowCustomEditor] = useState(false);
 
   return (
     <div className="flex flex-col items-center justify-center ">
-      <div className="absolute inset-x-64 inset-y-32">
-        <Draggable>
-          <div className="w-full h-full text-2xl bg-white rounded-xl">
-            <div className="flex flex-col place-items-center px-4">
-              <div className="text-xl">hi there</div>
-              <div className="w-full h-full">
-                <CodeMirror
-                  value={code}
-                  height="100%"
-                  theme="light"
-                  extensions={[javascript()]}
-                  onChange={(value, viewUpdate) => {
-                    console.log("value:", value);
-                    setCode(value);
-                  }}
-                  className="h-full"
-                />
-              </div>
-            </div>
-          </div>
-        </Draggable>
+      <div className="absolute bg-white right-4 top-4 p-2 rounded text-2xl">
+        <button onClick={() => setShowCustomEditor(!showCustomEditor)}>
+          {showCustomEditor ? <div>➡️ 💻</div> : <div>⬅️ 💻</div>}
+        </button>
       </div>
       <canvas id="myThreeJsCanvas" />
+      {showCustomEditor ? <CustomEditor /> : null}
     </div>
   );
 }
