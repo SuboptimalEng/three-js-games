9e28ec6401128b5a6e74509d8670f76c3211b07d
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-11-30 10:21:52 -0600

    Start setting up waveform.

---
+++ audio-waveforms/pages/index.jsx
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <div>hi</div>
+    </div>
+  );
+}
