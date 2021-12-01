7158dc474d711bb7201d18e76a2b000690e0e547
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-12-01 12:17:24 -0600

    Clean up code and remove blue boi mp3 file.

--- audio-waveform/pages/index.jsx
+++ audio-waveform/pages/index.jsx
@@ -1,72 +1,71 @@
 import { useEffect } from "react";
 import * as THREE from "three";
 import SceneInit from "./lib/SceneInit";
-import vertexShader from "./lib/Shaders";
+import { vertexShader, fragmentShader } from "./lib/Shaders";
 
 export default function Home() {
-  let test;
-  let audioContext, audioElement, dataArray, analyser, source;
-  let planeGeometry, planeMaterial, planeCustomMaterial, uniforms, planeMesh;
+  let test, audioContext, audioElement, dataArray, analyser, source;
+
+  const setupAudioContext = () => {
+    audioContext = new window.AudioContext();
+    audioElement = document.getElementById("myAudio");
+    source = audioContext.createMediaElementSource(audioElement);
+    analyser = audioContext.createAnalyser();
+    source.connect(analyser);
+    analyser.connect(audioContext.destination);
+    analyser.fftSize = 256;
+    dataArray = new Uint8Array(analyser.frequencyBinCount);
+  };
 
   const play = () => {
     if (audioContext === undefined) {
-      audioContext = new window.AudioContext();
-      audioElement = document.getElementById("myAudio");
-      source = audioContext.createMediaElementSource(audioElement);
-      analyser = audioContext.createAnalyser();
-      source.connect(analyser);
-      analyser.connect(audioContext.destination);
-      analyser.fftSize = 1024;
-      dataArray = new Uint8Array(analyser.frequencyBinCount);
-
-      uniforms = {
-        u_time: { value: 1.0 },
-        u_data_arr: {
-          type: "float[64]",
-          value: dataArray,
-        },
-        colorB: { type: "vec3", value: new THREE.Color(0xfff000) },
-        colorA: { type: "vec3", value: new THREE.Color(0x000fff) },
-      };
-
-      planeGeometry = new THREE.PlaneBufferGeometry(64, 64, 64, 64);
-      // planeGeometry = new THREE.BoxGeometry(64, 64, 16, 64, 64, 16);
-      // planeGeometry = new THREE.SphereGeometry(16, 16, 16);
-      // planeMaterial = new THREE.MeshNormalMaterial({ wireframe: true });
-      // planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
-      planeCustomMaterial = new THREE.ShaderMaterial({
-        uniforms: uniforms,
-        vertexShader: vertexShader(),
-        wireframe: true,
-      });
-      planeMesh = new THREE.Mesh(planeGeometry, planeCustomMaterial);
-      planeMesh.rotation.x = -Math.PI / 2 + Math.PI / 4;
-      planeMesh.scale.x = 2;
-      planeMesh.scale.y = 2;
-      planeMesh.scale.z = 2;
-      test.scene.add(planeMesh);
-      // let planeMesh2 = new THREE.Mesh(planeGeometry, planeCustomMaterial);
-      // planeMesh2.rotation.x = Math.PI / 2 + Math.PI / 4;
-      // planeMesh2.rotation.z = Math.PI / 2;
-      // planeMesh2.scale.x = 2;
-      // planeMesh2.scale.y = 2;
-      // planeMesh2.scale.z = 2;
-      // test.scene.add(planeMesh2);
-
-      const render = (t) => {
-        analyser.getByteFrequencyData(dataArray);
-        uniforms.u_time.value = t;
-        uniforms.u_data_arr.value = dataArray;
-        // let str = "";
-        // for (let i = 0; i < 64; i += 1) {
-        //   str += dataArray[i] + ",";
-        // }
-        // console.log(str);
-        requestAnimationFrame(render);
-      };
-
-      render();
+      setupAudioContext();
     }
+
+    const uniforms = {
+      u_time: { value: 1.0 },
+      u_data_arr: {
+        type: "float[64]",
+        value: dataArray,
+      },
+      // u_black: { type: "vec3", value: new THREE.Color(0x000000) },
+      // u_white: { type: "vec3", value: new THREE.Color(0xffffff) },
+    };
+
+    // note: uncomment these geometries to see different visualizations
+    // const planeGeometry = new THREE.BoxGeometry(64, 64, 8, 64, 64, 8);
+    // const planeGeometry = new THREE.SphereGeometry(16, 64, 64);
+
+    // note: set up plane mesh and add it to the scene
+    const planeGeometry = new THREE.PlaneGeometry(64, 64, 64, 64);
+    const planeCustomMaterial = new THREE.ShaderMaterial({
+      // note: this is where the magic happens
+      uniforms: uniforms,
+      vertexShader: vertexShader(),
+      fragmentShader: fragmentShader(),
+      wireframe: true,
+    });
+    const planeMesh = new THREE.Mesh(planeGeometry, planeCustomMaterial);
+    planeMesh.rotation.x = -Math.PI / 2 + Math.PI / 4;
+    planeMesh.scale.x = 2;
+    planeMesh.scale.y = 2;
+    planeMesh.scale.z = 2;
+    planeMesh.position.y = 5;
+    test.scene.add(planeMesh);
+
+    const render = (time) => {
+      // note: update audio data
+      analyser.getByteFrequencyData(dataArray);
+
+      // note: update uniforms
+      uniforms.u_time.value = time;
+      uniforms.u_data_arr.value = dataArray;
+
+      // note: call render function on every animation frame
+      requestAnimationFrame(render);
+    };
+
+    render();
   };
 
   useEffect(() => {
@@ -80,7 +79,7 @@ export default function Home() {
       <div className="absolute top-4 right-4">
         <audio
           id="myAudio"
-          src="./blue_boi.mp3"
+          src="./fur_elise.mp3"
           className="w-80"
           controls
           autoPlay
--- audio-waveform/pages/lib/Shaders.js
+++ audio-waveform/pages/lib/Shaders.js
@@ -3,11 +3,6 @@ const vertexShader = () => {
       varying float x;
       varying float y;
       varying float z;
-      varying float floor_x;
-      varying float floor_y;
-      varying float x_multiplier;
-      varying float y_multiplier;
-
       varying vec3 vUv;
 
       uniform float u_time;
@@ -19,47 +14,36 @@ const vertexShader = () => {
         x = abs(position.x);
 	      y = abs(position.y);
 
-        floor_x = round(x);
-	      floor_y = round(y);
+        float floor_x = round(x);
+	      float floor_y = round(y);
 
-        x_multiplier = (32.0 - x) / 8.0;
-        y_multiplier = (32.0 - y) / 8.0;
+        float x_multiplier = (32.0 - x) / 8.0;
+        float y_multiplier = (32.0 - y) / 8.0;
 
-        z = cos(u_data_arr[int(floor_x)] / 50.0 + u_data_arr[int(floor_y)] / 50.0);
-
-        // if (x >= 0.0) {
-        //   z = u_data_arr[0] / 100.0;
-        // }
-        // if (x >= 1.0) {
-        //   z = u_data_arr[1] / 100.0;
-        // }
-        // if (x >= 2.0) {
-        //   z = u_data_arr[2] / 100.0;
-        // }
-        // if (x >= 3.0) {
-        //   z = u_data_arr[3] / 100.0;
-        // }
-        // if (x >= 4.0) {
-        //   z = u_data_arr[4] / 100.0;
-        // }
-        // if (x >= 5.0) {
-        //   z = u_data_arr[5] / 100.0;
-        // }
-        // if (x >= 6.0) {
-        //   z = u_data_arr[6] / 100.0;
-        // }
-        // if (x >= 7.0) {
-        //   z = u_data_arr[7] / 100.0;
-        // }
-        // if (x >= 8.0) {
-        //   z = u_data_arr[8] / 100.0;
-        // }
-        // if (x >= 9.0) {
-        //   z = u_data_arr[9] / 100.0;
-        // }
+        z = cos(u_data_arr[int(floor_x)] / 50.0 + u_data_arr[int(floor_y)] / 50.0) * 2.0 + position.z / 2.0;
+        // z = (u_data_arr[int(floor_x)] / 50.0 + u_data_arr[int(floor_y)] / 50.0) * 2.0;
         gl_Position = projectionMatrix * modelViewMatrix * vec4(position.x, position.y, z, 1.0);
       }
     `;
 };
 
-export default vertexShader;
+const fragmentShader = () => {
+  return `
+    varying float x;
+    varying float y;
+    varying float z;
+    varying vec3 vUv;
+
+    uniform float u_time;
+    // uniform vec3 u_black;
+    // uniform vec3 u_white;
+
+    void main() {
+      // gl_FragColor = vec4(mix(u_black, u_white, vUv.x), 1.0);
+
+      gl_FragColor = vec4(sin(u_time / 1000.0) + (32.0 - abs(x)) / 32.0 - 1.0, (32.0 - abs(y)) / 32.0, (abs(x + y) / 2.0) / 32.0, 1.0);
+    }
+  `;
+};
+
+export { vertexShader, fragmentShader };
