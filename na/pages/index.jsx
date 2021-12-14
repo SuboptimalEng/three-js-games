2c68879a02f2cbd6743da261832ac2f90ea8bfd7
suboptimaleng <suboptimaleng@gmail.com>
2021-12-13 20:19:54 -0600

    Create naruto rasengan animation.

--- na/pages/index.jsx
+++ na/pages/index.jsx
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { useEffect } from "react";
 import SceneInit from "./lib/SceneInit";
-import { vertexShader, fragmentShader } from "./lib/Shaders";
+import { vertexShader, fragmentShader, vs, fs } from "./lib/Shaders";
 
 export default function Home() {
   useEffect(() => {
@@ -9,25 +9,22 @@ export default function Home() {
     test.initScene();
     test.animate();
 
-    const sphereGeometry = new THREE.SphereGeometry(8);
-    const sphereMaterial = new THREE.MeshToonMaterial();
-    const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
-    test.scene.add(sphereMesh);
-
-    const sg = new THREE.SphereGeometry(40, 20, 20);
-    const sm = new THREE.MeshNormalMaterial({
+    const sphereGeometry = new THREE.SphereGeometry(40, 20, 20);
+    const sphereMaterial = new THREE.MeshNormalMaterial({
       // wireframe: true,
       transparent: true,
-      opacity: 0.5,
+      opacity: 0.25,
     });
-    const sm2 = new THREE.Mesh(sg, sm);
-    test.scene.add(sm2);
+    const sm = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    test.scene.add(sm);
 
-    const boxGeometry = new THREE.BoxGeometry(4, 4, 72, 1, 1, 16);
+    const g = new THREE.Group();
+
+    const boxGeometry = new THREE.BoxGeometry(4, 4, 120, 1, 1, 16);
     const boxMaterial = new THREE.ShaderMaterial({
       vertexShader: vertexShader(),
       fragmentShader: fragmentShader(),
-      wireframe: true,
+      // wireframe: true,
     });
     const bm = new THREE.Mesh(boxGeometry, boxMaterial);
     bm.rotation.x = Math.PI / 16;
@@ -62,6 +59,42 @@ export default function Home() {
     bm6.rotation.x = Math.PI / 3;
     test.scene.add(bm6);
 
+    const bm7 = new THREE.Mesh(boxGeometry, boxMaterial);
+    bm7.rotation.x = Math.PI / 5;
+    test.scene.add(bm7);
+
+    const bm8 = new THREE.Mesh(boxGeometry, boxMaterial);
+    bm8.rotation.x = Math.PI / 7;
+    test.scene.add(bm8);
+
+    const bm9 = new THREE.Mesh(boxGeometry, boxMaterial);
+    bm9.rotation.x = Math.PI / 2.5;
+    test.scene.add(bm9);
+
+    const bm10 = new THREE.Mesh(boxGeometry, boxMaterial);
+    bm10.rotation.x = Math.PI / 3.5;
+    test.scene.add(bm10);
+
+    g.add(bm, bm2, bm3, bm4, bm5, bm6, bm7, bm8, bm9, bm10);
+    test.scene.add(g);
+
+    const sphereGeometry2 = new THREE.SphereGeometry(40, 40, 40);
+    const sphereMaterial2 = new THREE.ShaderMaterial({
+      vertexShader: vs(),
+      fragmentShader: fs(),
+      blending: THREE.AdditiveBlending,
+      side: THREE.BackSide,
+      // transparent: true,
+      // opacity: 0.5,
+      // wireframe: true,
+    });
+    const sm2 = new THREE.Mesh(sphereGeometry2, sphereMaterial2);
+    console.log(sm2);
+    sm2.scale.x = 1.25;
+    sm2.scale.y = 1.25;
+    sm2.scale.z = 1.25;
+    test.scene.add(sm2);
+
     const render = () => {
       bm.rotation.y += 0.02;
       bm2.rotation.y += 0.024;
@@ -69,7 +102,11 @@ export default function Home() {
       bm4.rotation.y += 0.032;
       bm5.rotation.y += 0.036;
       bm6.rotation.y += 0.04;
-      // sm2.rotation.y += 0.01;
+      bm7.rotation.y += 0.044;
+      bm8.rotation.y += 0.048;
+      bm9.rotation.y += 0.052;
+      bm10.rotation.y += 0.056;
+      g.rotation.y += 0.01;
 
       window.requestAnimationFrame(render);
     };
--- na/pages/lib/Shaders.js
+++ na/pages/lib/Shaders.js
@@ -5,14 +5,35 @@ const vertexShader = () => {
     void main() {
       vUv = position;
 
-      gl_Position = projectionMatrix
-        * modelViewMatrix
-        * vec4(
-          sin(position.z / 16.0) * 16.0 + position.x * 4.0 / position.z,
-          position.y * 4.0 / position.z,
-          position.z,
-          1.0
-        );
+      // gl_Position = projectionMatrix
+      //   * modelViewMatrix
+      //   * vec4(
+      //     sin(position.z / 16.0) * 16.0 + position.x * 4.0 / abs(position.z) ,
+      //     position.y * 4.0 / position.z,
+      //     position.z,
+      //     1.0
+      //   );
+
+      if (position.z == 0.0) {
+        gl_Position = projectionMatrix
+          * modelViewMatrix
+          * vec4(
+            position.x * 0.5,
+            position.y * 0.5,
+            position.z,
+            1.0
+          );
+      } else {
+        // position.x / position.z,
+        gl_Position = projectionMatrix
+          * modelViewMatrix
+          * vec4(
+            sin(position.z / 16.0) * 16.0 + position.x * 0.5 / abs(position.z) ,
+            position.y * 0.5 / abs(position.z),
+            position.z,
+            1.0
+          );
+      }
     }
   `;
 };
@@ -22,9 +43,37 @@ const fragmentShader = () => {
     varying vec3 vUv;
 
     void main() {
-      gl_FragColor = vec4(max(0.5, abs(sin(vUv.z / 40.0))), max(0.75, abs(sin(vUv.z / 40.0))), 1.0, 1.0);
+      gl_FragColor = vec4(max(0.6, abs(sin(vUv.z / 20.0))), max(0.8, abs(sin(vUv.z / 20.0))), 1.0, 1.0);
     }
   `;
 };
 
-export { vertexShader, fragmentShader };
+const vs = () => {
+  return `
+    varying vec2 vertexUv;
+    varying vec3 vertexNormal;
+
+    void main() {
+      vertexUv = uv;
+      vertexNormal = normalize(normalMatrix * normal);
+
+      gl_Position = projectionMatrix
+          * modelViewMatrix
+          * vec4(position.x, position.y, position.z, 1.0);
+    }
+  `;
+};
+
+const fs = () => {
+  return `
+    varying vec2 vertexUv;
+    varying vec3 vertexNormal;
+
+    void main() {
+      float intensity = pow(0.5 - dot(vertexNormal, vec3(0.0, 0.0, 1.0)), 2.0);
+      gl_FragColor = vec4(0.5, 0.75, 1.0, 1.0) * intensity;
+    }
+  `;
+};
+
+export { vertexShader, fragmentShader, vs, fs };
