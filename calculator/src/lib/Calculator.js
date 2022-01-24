75bf6bd8ed73fc74f354739ff9ffd35a49a690c7
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-01-24 12:10:41 -0600

    Clean up load text function.

--- calculator/src/lib/Calculator.js
+++ calculator/src/lib/Calculator.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
 import { TTFLoader } from 'three/examples/jsm/loaders/TTFLoader';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
 import { RoundedBoxGeometry } from 'three/examples/jsm/geometries/RoundedBoxGeometry';
@@ -12,35 +11,33 @@ export default class Calculator {
     this.calculatorGroup = new THREE.Group();
     this.loadCalculatorMesh();
     this.loadKeyMeshes();
-    this.loadTextMeshes();
+    this.loadKeyText();
   }
 
-  loadTextMeshes() {
+  loadKeyText() {
     this.ttfLoader = new TTFLoader();
     this.fontLoader = new FontLoader();
     this.ttfLoader.load('./fonts/jbm-regular.ttf', (unparsedFont) => {
-      const font = this.fontLoader.parse(unparsedFont);
+      const parsedFont = this.fontLoader.parse(unparsedFont);
       this.keys.forEach((key) => {
-        const textGeo = new TextGeometry(key.text, {
-          font: font,
-          size: 8,
-          height: 2.5,
-        });
-        const textMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000 });
-        const textMesh = new THREE.Mesh(textGeo, textMaterial);
-        textMesh.position.x = key.xOffset - 4;
-        textMesh.position.y = key.yOffset - 4;
-        textMesh.position.z = 10;
-        key.keyGroup.add(textMesh);
+        key.addText(parsedFont);
       });
     });
   }
 
+  loadKeyMeshes() {
+    const k1 = new Key('1', 1.5, -36, 0);
+    const k2 = new Key('2', 1.5, -12, 0);
+    const k3 = new Key('3', 1.5, 12, 0);
+    const k4 = new Key('+', 1.5, 36, 0);
+    this.keys = [k1, k2, k3, k4];
+    this.keys.forEach((k) => this.calculatorGroup.add(k.keyGroup));
+  }
+
   loadCalculatorMesh() {
-    const geometry = new RoundedBoxGeometry(100, 100, 8, 4, 0.5);
+    const geometry = new RoundedBoxGeometry(100, 100, 4, 4, 0.5);
     const material = new THREE.MeshNormalMaterial();
     this.calculatorMesh = new THREE.Mesh(geometry, material);
-    this.calculatorMesh.position.y = -8;
     this.calculatorGroup.add(this.calculatorMesh);
   }
 
@@ -57,14 +54,5 @@ export default class Calculator {
     if (validKey) {
       validKey.up();
     }
-  }
-
-  loadKeyMeshes() {
-    const k1 = new Key('1', 1.5, -36, 0);
-    const k2 = new Key('2', 1.5, -12, 0);
-    const k3 = new Key('3', 1.5, 12, 0);
-    const k4 = new Key('+', 1.5, 36, 0);
-    this.keys = [k1, k2, k3, k4];
-    this.keys.forEach((k) => this.calculatorGroup.add(k.keyGroup));
   }
 }
--- calculator/src/lib/Key.js
+++ calculator/src/lib/Key.js
@@ -1,11 +1,16 @@
 import * as THREE from 'three';
 
+// import { keyVertexShader, keyFragmentShader } from './Shaders';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import { RoundedBoxGeometry } from 'three/examples/jsm/geometries/RoundedBoxGeometry';
+
 export default class Key {
   constructor(text, width, xOffset, yOffset) {
     this.text = text;
     this.width = width;
     this.xOffset = xOffset;
     this.yOffset = yOffset;
+    this.zOffset = 4.5;
 
     this.keyGroup = new THREE.Group();
     this.loadKeyMesh();
@@ -16,24 +21,40 @@ export default class Key {
   }
 
   down() {
-    this.keyGroup.position.z = -4;
+    this.keyGroup.position.z = -2;
+  }
+
+  addText(parsedFont) {
+    const textGeometry = new TextGeometry(this.text, {
+      font: parsedFont,
+      size: 8,
+      height: 2.5,
+    });
+    const textMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+    this.textMesh = new THREE.Mesh(textGeometry, textMaterial);
+
+    this.textMesh.position.x = this.xOffset - 4;
+    this.textMesh.position.y = this.yOffset - 4;
+    this.textMesh.position.z = this.zOffset;
+    this.keyGroup.add(this.textMesh);
   }
 
   loadKeyMesh() {
-    const geometry = new THREE.BoxGeometry(64, 64, 32);
+    const geometry = new RoundedBoxGeometry(16, 16, 4, 2, 1);
     const material = new THREE.MeshStandardMaterial({ color: '#fafafa' });
     // TODO: Create fancy key.
+    // const geometry = new THREE.BoxGeometry(64, 64, 32);
     // const material = new THREE.ShaderMaterial({
     //   wireframe: true,
     //   vertexShader: keyVertexShader(),
     //   fragmentShader: keyFragmentShader(),
     // });
+    // this.keyMesh.scale.set(0.25, 0.25, 0.25);
     this.keyMesh = new THREE.Mesh(geometry, material);
     this.keyMesh.position.x = this.xOffset;
     this.keyMesh.position.y = this.yOffset;
-    this.keyMesh.position.z = 8;
+    this.keyMesh.position.z = this.zOffset;
     this.keyMesh.rotation.x = Math.PI;
-    this.keyMesh.scale.set(0.25, 0.25, 0.25);
 
     this.keyGroup.add(this.keyMesh);
   }
