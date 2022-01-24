9597a3ed23901030a6275442856b862bf092bb23
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-01-24 11:40:43 -0600

    Add Jet Brains Mono text on calculator buttons.

--- calculator/src/lib/Calculator.js
+++ calculator/src/lib/Calculator.js
@@ -1,4 +1,8 @@
 import * as THREE from 'three';
+
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import { TTFLoader } from 'three/examples/jsm/loaders/TTFLoader';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
 import { RoundedBoxGeometry } from 'three/examples/jsm/geometries/RoundedBoxGeometry';
 
 import Key from './Key';
@@ -6,9 +10,30 @@ import Key from './Key';
 export default class Calculator {
   constructor() {
     this.calculatorGroup = new THREE.Group();
-
     this.loadCalculatorMesh();
     this.loadKeyMeshes();
+    this.loadTextMeshes();
+  }
+
+  loadTextMeshes() {
+    this.ttfLoader = new TTFLoader();
+    this.fontLoader = new FontLoader();
+    this.ttfLoader.load('./fonts/jbm-regular.ttf', (unparsedFont) => {
+      const font = this.fontLoader.parse(unparsedFont);
+      this.keys.forEach((key) => {
+        const textGeo = new TextGeometry(key.text, {
+          font: font,
+          size: 8,
+          height: 2.5,
+        });
+        const textMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+        const textMesh = new THREE.Mesh(textGeo, textMaterial);
+        textMesh.position.x = key.xOffset - 4;
+        textMesh.position.y = key.yOffset - 4;
+        textMesh.position.z = 10;
+        key.keyGroup.add(textMesh);
+      });
+    });
   }
 
   loadCalculatorMesh() {
@@ -35,10 +60,11 @@ export default class Calculator {
   }
 
   loadKeyMeshes() {
-    const k1 = new Key('1', 1.5, -20, 0);
-    const k2 = new Key('2', 1.5, 0, 0);
-    const k3 = new Key('3', 1.5, 20, 0);
-    this.keys = [k1, k2, k3];
+    const k1 = new Key('1', 1.5, -36, 0);
+    const k2 = new Key('2', 1.5, -12, 0);
+    const k3 = new Key('3', 1.5, 12, 0);
+    const k4 = new Key('+', 1.5, 36, 0);
+    this.keys = [k1, k2, k3, k4];
     this.keys.forEach((k) => this.calculatorGroup.add(k.keyGroup));
   }
 }
--- calculator/src/lib/Key.js
+++ calculator/src/lib/Key.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three';
-import { keyFragmentShader, keyVertexShader } from './shaders';
 
 export default class Key {
   constructor(text, width, xOffset, yOffset) {
@@ -21,17 +20,19 @@ export default class Key {
   }
 
   loadKeyMesh() {
-    const geometry = new THREE.BoxGeometry(32, 32, 32);
-    const material = new THREE.ShaderMaterial({
-      wireframe: true,
-      vertexShader: keyVertexShader(),
-      fragmentShader: keyFragmentShader(),
-    });
+    const geometry = new THREE.BoxGeometry(64, 64, 32);
+    const material = new THREE.MeshStandardMaterial({ color: '#fafafa' });
+    // TODO: Create fancy key.
+    // const material = new THREE.ShaderMaterial({
+    //   wireframe: true,
+    //   vertexShader: keyVertexShader(),
+    //   fragmentShader: keyFragmentShader(),
+    // });
     this.keyMesh = new THREE.Mesh(geometry, material);
     this.keyMesh.position.x = this.xOffset;
     this.keyMesh.position.y = this.yOffset;
-    this.keyMesh.position.z = 16;
-    this.keyMesh.rotation.x = Math.PI / 2;
+    this.keyMesh.position.z = 8;
+    this.keyMesh.rotation.x = Math.PI;
     this.keyMesh.scale.set(0.25, 0.25, 0.25);
 
     this.keyGroup.add(this.keyMesh);
