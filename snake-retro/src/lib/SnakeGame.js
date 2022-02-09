e7767ae34802a9710b4bf0af4d941b095158e83b
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-02-09 16:56:14 -0600

    Add comments to snake game.

--- snake-retro/src/lib/SnakeGame.js
+++ snake-retro/src/lib/SnakeGame.js
@@ -2,18 +2,23 @@ import * as THREE from 'three';
 
 export default class SnakeGame {
   constructor() {
-    // Board info.
+    // NOTE: Changeable board constants.
     this.boardSize = 8;
     this.boardScale = 5;
 
     // NOTE: This group 'g' will contain the entire snake game.
     this.g = new THREE.Group();
 
+    // NOTE: This 'boardGroup' is a wrapper for the board tiles.
+    // NOTE: It is helpful to use a groups to keep track of common structures.
+    // NOTE: E.g. This group makes it easy to reset the board + change its scale.
+    this.boardGroup = new THREE.Group();
+
     this.resetBoard();
-    // const geometry = new THREE.BoxGeometry(16, 16, 16);
-    // const material = new THREE.MeshNormalMaterial();
-    // const mesh = new THREE.Mesh(geometry, material);
-    // this.g.add(mesh);
+  }
+
+  resetBoardGroup() {
+    this.boardGroup.clear();
   }
 
   resetBoardScale() {
@@ -25,22 +30,20 @@ export default class SnakeGame {
   }
 
   resetBoard() {
-    if (this.boardGroup && this.boardGroup.children.length > 0) {
-      this.boardGroup.clear();
-    } else {
-      this.boardGroup = new THREE.Group();
-    }
+    this.resetBoardGroup();
     this.resetBoardScale();
+
     for (let i = 0; i < this.boardSize; i++) {
       for (let j = 0; j < this.boardSize; j++) {
         const geometry = new THREE.BoxGeometry(1, 1, 1);
         const material = new THREE.MeshNormalMaterial({ wireframe: true });
         const boardTile = new THREE.Mesh(geometry, material);
+
+        // NOTE: Position the board tiles in the center of the screen.
         boardTile.position.x = i - this.boardSize / 2 + 0.5;
         boardTile.position.y = j - this.boardSize / 2 + 0.5;
         this.boardGroup.add(boardTile);
       }
     }
-    this.g.add(this.boardGroup);
   }
 }
